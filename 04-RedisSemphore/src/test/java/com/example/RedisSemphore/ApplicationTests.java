a42221d938d6d1a708cded02a4e1a8d8f6d456d1
MASIJUN <785493116@qq.com>
2021-07-26 21:27:45 +0800

    fix maven project

---
+++ 04-RedisSemphore/src/test/java/com/example/RedisSemphore/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RedisSemphore;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
